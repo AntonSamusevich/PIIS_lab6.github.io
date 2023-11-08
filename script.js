5aced36ff0a742a53d231fbabf1d6ff1f2bcc34c
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-09 00:44:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,77 @@
 document.addEventListener('DOMContentLoaded', function () {
   const targets = document.querySelectorAll('.target'); 
   
-  let activeElement = null; //активный элемент
-  let flag = false; //состояние перемещения элемента
-  let offsetX, offsetY; //смещение относительно курсора
-  let startPosition = null; //исходная позиция элемента
+  let activeElement = null; // Активный элемент
+  let flag = false; // Состояние перемещения элемента
+  let offsetX, offsetY; // Смещение относительно курсора
+  let startPosition = null; // Исходная позиция элемента
+  let lastTouchStart = 0; // Время последнего touchstart
 
   targets.forEach(target => {
 
-    // Обработчик события касания начала
+    // Обработчик события touchstart
     target.addEventListener('touchstart', (e) => {
-      activeElement = target;
-      startPosition = {
-        left: target.style.left,
-        top: target.style.top,
-      };
-      const touch = e.touches[0];
-      offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
-      offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
-      e.preventDefault(); // Предотвращаем дефолтное действие браузера
+      const currentTime = new Date().getTime();
+      if (currentTime - lastTouchStart < 1000) {
+        // Если прошло менее 1 секунды с последнего touchstart, считаем это двойным нажатием
+        if (activeElement) {
+          activeElement.style.backgroundColor = 'green';
+          e.preventDefault();
+        } else {
+          activeElement = target;
+          startPosition = {
+            left: target.style.left,
+            top: target.style.top,
+          };
+          activeElement.style.backgroundColor = 'green';
+          const touch = e.touches[0];
+          offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
+          offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
+          e.preventDefault();
+        }
+      } else {
+        lastTouchStart = currentTime;
+        if (activeElement) {
+          activeElement.style.backgroundColor = 'red';
+          e.preventDefault();
+        } else {
+          activeElement = target;
+          startPosition = {
+            left: target.style.left,
+            top: target.style.top,
+          };
+          activeElement.style.backgroundColor = 'red';
+          const touch = e.touches[0];
+          offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
+          offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
+          e.preventDefault();
+        }
+      }
     });
   });
 
-  // Обработчик события движения при касании
+  // Обработчик события touchmove
   document.addEventListener('touchmove', (e) => {
     if (activeElement) {
       const touch = e.touches[0];
       activeElement.style.left = touch.clientX - offsetX + 'px'; 
       activeElement.style.top = touch.clientY - offsetY + 'px';
-      e.preventDefault(); // Предотвращаем дефолтное действие браузера
+      e.preventDefault();
     }
   });
 
-  // Обработчик события завершения касания
+  // Обработчик события touchend
   document.addEventListener('touchend', (e) => {
+    lastTouchStart = 0;
     if (flag) {
       flag = false; // Прерываем перетаскивание
     } else {
-      activeElement = null; // Сбрасываем активный элемент
+      activeElement = null;
     }
   });
 
-  // Обработчик события касания вторым пальцем
-  document.addEventListener('touchstart', (e) => {
+   // Обработчик события касания вторым пальцем
+   document.addEventListener('touchstart', (e) => {
     if (activeElement && e.touches.length === 2) {
       activeElement.style.backgroundColor = 'red';
       activeElement.style.left = startPosition.left;
