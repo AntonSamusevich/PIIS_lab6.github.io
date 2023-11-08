3cfae6a03abe5aa0d94c1032c11951afd6dfb7d2
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-09 01:45:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,24 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
   targets.forEach(target => {
     // Обработчик события касания начала
     target.addEventListener('touchstart', (e) => {
-      const currentTime = new Date().getTime();
-      if (touchCount === 0 || (currentTime - touchStartTime < 150)) {
-        // Если прошло менее 0.3 секунды с начала первого касания, увеличиваем счетчик
-        touchCount++;
-        if (touchCount === 2) {
-          // Если счетчик достиг двух, считаем это двойным нажатием
-          touchCount = 0;
-          activeElement = target;
-          startPosition = {
-            left: target.style.left,
-            top: target.style.top,
-          };
-          activeElement.style.backgroundColor = 'green';
-        } else {
-          touchStartTime = currentTime;
-        }
-      } else {
-        touchCount = 0;
+      if (touchCount === 0) {
+        // Если это первое касание, начинаем перетаскивание
         activeElement = target;
         startPosition = {
           left: target.style.left,
@@ -37,8 +21,18 @@ document.addEventListener('DOMContentLoaded', function () {
         const touch = e.touches[0];
         offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
         offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
-        e.preventDefault(); 
+      } else if (touchCount === 1) {
+        // Если это второе касание, считаем это двойным нажатием и меняем цвет
+        touchCount = 0;
+        activeElement = target;
+        startPosition = {
+          left: target.style.left,
+          top: target.style.top,
+        };
+        activeElement.style.backgroundColor = 'green';
       }
+      touchCount++;
+      e.preventDefault();
     });
 
     // Обработчик события движения при касании
