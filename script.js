f71a4d179e7f38e5c3b2c31bf93bb0db173fb9de
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-09 01:05:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,76 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const targets = document.querySelectorAll('.target');
+  const targets = document.querySelectorAll('.target'); 
   
-  let activeElement = null; // Активный элемент
-  let flag = false; // Состояние перемещения элемента
-  let offsetX, offsetY; // Смещение относительно курсора
-  let startPosition = null; // Исходная позиция элемента
-  let lastTouchStart = 0; // Время последнего touchstart
+  let activeElement = null; //активный элемент
+  let flag = false; //состояние перемещения элемента
+  let offsetX, offsetY; //смещение относительно курсора
+  let startPosition = null; //исходная позиция элемента
+  let touchCount = 0; // Счетчик касаний
+  let touchStartTime = 0; // Время начала первого касания
 
   targets.forEach(target => {
 
-    // Обработчик события touchstart
+    // Обработчик события касания начала
     target.addEventListener('touchstart', (e) => {
       const currentTime = new Date().getTime();
-      if (currentTime - lastTouchStart < 1000) {
-        // Если прошло менее 1 секунды с последнего touchstart, считаем это двойным нажатием
-        if (activeElement) {
-          activeElement.style.backgroundColor = 'green';
-          e.preventDefault();
-          flag = true;
-        }
-      } else {
-        if (activeElement) {
-          activeElement.style.backgroundColor = 'red';
-          e.preventDefault();
-          flag = true;
-        } else {
+      if (touchCount === 0 || (currentTime - touchStartTime < 1000)) {
+        // Если прошло менее 1 секунды с начала первого касания, увеличиваем счетчик
+        touchCount++;
+        if (touchCount === 2) {
+          // Если счетчик достиг двух, считаем это двойным нажатием
+          touchCount = 0;
           activeElement = target;
           startPosition = {
             left: target.style.left,
             top: target.style.top,
           };
-          activeElement.style.backgroundColor = 'red';
+          activeElement.style.backgroundColor = 'green';
           const touch = e.touches[0];
           offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
           offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
           e.preventDefault();
         }
+      } else {
+        activeElement = target;
+        startPosition = {
+          left: target.style.left,
+        top: target.style.top,
+      };
+      const touch = e.touches[0];
+      offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
+      offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
+      e.preventDefault(); // Предотвращаем дефолтное действие браузера
       }
-      lastTouchStart = currentTime;
     });
   });
 
-  // Обработчик события touchmove
+  // Обработчик события движения при касании
   document.addEventListener('touchmove', (e) => {
     if (activeElement) {
       const touch = e.touches[0];
-      activeElement.style.left = touch.clientX - offsetX + 'px';
+      activeElement.style.left = touch.clientX - offsetX + 'px'; 
       activeElement.style.top = touch.clientY - offsetY + 'px';
-      e.preventDefault();
+      e.preventDefault(); // Предотвращаем дефолтное действие браузера
     }
   });
 
-  // Обработчик события touchend
+  // Обработчик события завершения касания
   document.addEventListener('touchend', (e) => {
     if (flag) {
       flag = false; // Прерываем перетаскивание
     } else {
-      activeElement = null;
+      activeElement = null; // Сбрасываем активный элемент
+    }
+  });
+
+  // Обработчик события касания вторым пальцем
+  document.addEventListener('touchstart', (e) => {
+    if (activeElement && e.touches.length === 2) {
+      flag = false; // Прерываем перетаскивание
+      activeElement.style.left = startPosition.left;
+      activeElement.style.top = startPosition.top;
+      activeElement = null; // Сбрасываем активный элемент
+      e.preventDefault(); // Предотвращаем дефолтное действие браузера
     }
   });
 });
