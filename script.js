de80775498f5522ee78040f8e8dd464b25087f3b
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-09 21:28:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
   const targets = document.querySelectorAll('.target'); 
   
-  let activeElement = null; // Активный элемент
-  let flag = false; // Состояние перемещения элемента
-  let offsetX, offsetY; // Смещение относительно курсора
-  let startPosition = null; // Исходная позиция элемента
-  let touchCount = 0; // Счетчик касаний
-  let touchStartTime = 0; // Время начала первого касания
+  let activeElement = null; //активный элемент
+  let flag = false; //состояние перемещения элемента
+  let offsetX, offsetY; //смещение относительно курсора
+  let startPosition = null; //исходная позиция элемента
 
   targets.forEach(target => {
 
-    // Обработчик события касания начала
     target.addEventListener('touchstart', (e) => {
       const currentTime = new Date().getTime();
-      if (touchCount === 0 || (currentTime - touchStartTime < 1000)) {
-        // Если прошло менее 1 секунды с начала первого касания, увеличиваем счетчик
+      if (touchCount === 0 || (currentTime - touchStartTime < 300)) {
+        // Если прошло менее 0.3 секунды с начала первого касания, увеличиваем счетчик
         touchCount++;
         if (touchCount === 2) {
           // Если счетчик достиг двух, считаем это двойным нажатием
           touchCount = 0;
+          activeElement = target;
+          startPosition = {
+            left: target.style.left,
+            top: target.style.top,
+          };
           activeElement.style.backgroundColor = 'green';
         } else {
           touchStartTime = currentTime;
         }
       } else {
+        touchCount = 0;
         activeElement = target;
         startPosition = {
           left: target.style.left,
@@ -60,6 +63,7 @@ document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('touchstart', (e) => {
     if (activeElement && e.touches.length === 2) {
       flag = false; // Прерываем перетаскивание
+      activeElement.style.backgroundColor = 'red';
       activeElement.style.left = startPosition.left;
       activeElement.style.top = startPosition.top;
       activeElement = null; // Сбрасываем активный элемент
