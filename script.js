c7a8e1b7cb5fec66107243b7f64bf96d45894549
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-10 00:07:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,10 +13,8 @@ document.addEventListener('DOMContentLoaded', function () {
     target.addEventListener('touchstart', (e) => {
       const currentTime = new Date().getTime();
       if (touchCount === 0 || (currentTime - touchStartTime < 300)) {
-        // Если прошло менее 0.3 секунды с начала первого касания, увеличиваем счетчик
         touchCount++;
         if (touchCount === 2) {
-          // Если счетчик достиг двух, считаем это двойным нажатием
           touchCount = 0;
           activeElement = target;
           startPosition = {
@@ -35,11 +33,11 @@ document.addEventListener('DOMContentLoaded', function () {
           top: target.style.top,
         };
         activeElement.style.backgroundColor = 'red';
-      }
         const touch = e.touches[0];
         offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
         offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
         e.preventDefault(); 
+      }
     });
 
     // Обработчик события движения при касании
