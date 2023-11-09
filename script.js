e4ef3386972f1a287fc911107ef5aa2e0b11bd31
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-09 23:37:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,34 +7,57 @@ document.addEventListener('DOMContentLoaded', function () {
   let pressStartTime = 0; // Время начала удержания пальца
 
   targets.forEach(target => {
+
     target.addEventListener('touchstart', (e) => {
       const currentTime = new Date().getTime();
       pressStartTime = currentTime;
-
-      if (currentTime - pressStartTime > 1000) {
+      if (touchCount === 0 || (currentTime - touchStartTime < 300)) {
+        // Если прошло менее 0.3 секунды с начала первого касания, увеличиваем счетчик
+        touchCount++;
+        if (touchCount === 2) {
+          // Если счетчик достиг двух, считаем это двойным нажатием
+          touchCount = 0;
+          activeElement = target;
+          startPosition = {
+            left: target.style.left,
+            top: target.style.top,
+          };
+          activeElement.style.backgroundColor = 'green';
+        } else {
+          touchStartTime = currentTime;
+        }
+      } else {
+        touchCount = 0;
         activeElement = target;
         startPosition = {
-          left: activeElement.style.left,
-          top: activeElement.style.top,
-        };
+          left: target.style.left,
+          top: target.style.top,
+        };}
         const touch = e.touches[0];
         offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
         offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
         e.preventDefault();
-      }
     });
 
+    // Обработчик события движения при касании
     document.addEventListener('touchmove', (e) => {
-      if (activeElement) {
-        const touch = e.touches[0];
-        activeElement.style.left = touch.clientX - offsetX + 'px';
-        activeElement.style.top = touch.clientY - offsetY + 'px';
-        e.preventDefault();
-      }
+      const touch = e.touches[0];
+      activeElement.style.left = touch.clientX - offsetX + 'px';
+      activeElement.style.top = touch.clientY - offsetY + 'px';
     });
 
+    // Обработчик события завершения касания
     document.addEventListener('touchend', (e) => {
-      activeElement = null;
+      activeElement = null; // Сбрасываем активный элемент
+    });
+
+    // Обработчик события касания вторым пальцем
+    document.addEventListener('touchstart', (e) => {
+      if (activeElement && e.touches.length === 2) {
+        activeElement.style.left = startPosition.left;
+        activeElement.style.top = startPosition.top;
+        activeElement = null; // Сбрасываем активный элемент
+      }
     });
   });
 });
