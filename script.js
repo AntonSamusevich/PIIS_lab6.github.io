dce15e138e1e48b926f8380307d98fbd8b9af77d
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-09 12:38:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,25 +11,15 @@ document.addEventListener('DOMContentLoaded', function () {
   targets.forEach(target => {
     // Обработчик события касания начала
     target.addEventListener('touchstart', (e) => {
-      const currentTime = new Date().getTime();
-      if (touchCount === 0) {
-        // Если touchCount равен 0, это первое касание, начинаем перемещение элемента
-        touchCount = 1;
-        activeElement = target;
-        startPosition = {
-          left: target.style.left,
-          top: target.style.top,
-        };
-        const touch = e.touches[0];
-        offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
-        offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
-        e.preventDefault();
-      } else if (touchCount === 1 && currentTime - touchStartTime < 2000) {
-        // Если touchCount равен 1 и прошло менее 0.3 секунд с начала первого касания, считаем это двойным нажатием
-        touchCount = 0;
-        activeElement.style.backgroundColor = 'green';
+      if (e.detail === 2) {
+          touchCount = 0;
+          activeElement = target;
+          startPosition = {
+            left: target.style.left,
+            top: target.style.top,
+          };
+          activeElement.style.backgroundColor = 'green';
       } else {
-        // В остальных случаях (touchCount === 1 и прошло более 0.3 секунд) сбрасываем touchCount и начинаем перемещение снова
         touchCount = 0;
         activeElement = target;
         startPosition = {
@@ -39,11 +29,10 @@ document.addEventListener('DOMContentLoaded', function () {
         const touch = e.touches[0];
         offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
         offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
-        e.preventDefault();
+        e.preventDefault(); 
       }
-      touchStartTime = currentTime;
     });
-    
+
     // Обработчик события движения при касании
     document.addEventListener('touchmove', (e) => {
       if (activeElement) {
