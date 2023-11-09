960385e1b8977b83fb6998737925a01222952a68
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-09 21:32:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
   const targets = document.querySelectorAll('.target'); 
   
-  let activeElement = null; //активный элемент
-  let flag = false; //состояние перемещения элемента
-  let offsetX, offsetY; //смещение относительно курсора
-  let startPosition = null; //исходная позиция элемента
+  let activeElement = null; // Активный элемент
+  let flag = false; // Состояние перемещения элемента
+  let offsetX, offsetY; // Смещение относительно курсора
+  let startPosition = null; // Исходная позиция элемента
+  let touchCount = 0; // Счетчик касаний
+  let touchStartTime = 0; // Время начала первого касания
 
   targets.forEach(target => {
-
+    // Обработчик события касания начала
     target.addEventListener('touchstart', (e) => {
       const currentTime = new Date().getTime();
       if (touchCount === 0 || (currentTime - touchStartTime < 300)) {
@@ -38,36 +40,35 @@ document.addEventListener('DOMContentLoaded', function () {
         e.preventDefault(); 
       }
     });
-  });
 
-  // Обработчик события движения при касании
-  document.addEventListener('touchmove', (e) => {
-    if (activeElement) {
-      const touch = e.touches[0];
-      activeElement.style.left = touch.clientX - offsetX + 'px'; 
-      activeElement.style.top = touch.clientY - offsetY + 'px';
-      e.preventDefault(); // Предотвращаем дефолтное действие браузера
-    }
-  });
+    // Обработчик события движения при касании
+    document.addEventListener('touchmove', (e) => {
+      if (activeElement) {
+        const touch = e.touches[0];
+        activeElement.style.left = touch.clientX - offsetX + 'px'; 
+        activeElement.style.top = touch.clientY - offsetY + 'px';
+        e.preventDefault(); // Предотвращаем дефолтное действие браузера
+      }
+    });
 
-  // Обработчик события завершения касания
-  document.addEventListener('touchend', (e) => {
-    if (flag) {
-      flag = false; // Прерываем перетаскивание
-    } else {
-      activeElement = null; // Сбрасываем активный элемент
-    }
-  });
+    // Обработчик события завершения касания
+    document.addEventListener('touchend', (e) => {
+      if (flag) {
+        flag = false; // Прерываем перетаскивание
+      } else {
+        activeElement = null; // Сбрасываем активный элемент
+      }
+    });
 
-  // Обработчик события касания вторым пальцем
-  document.addEventListener('touchstart', (e) => {
-    if (activeElement && e.touches.length === 2) {
-      flag = false; // Прерываем перетаскивание
-      activeElement.style.backgroundColor = 'red';
-      activeElement.style.left = startPosition.left;
-      activeElement.style.top = startPosition.top;
-      activeElement = null; // Сбрасываем активный элемент
-      e.preventDefault(); // Предотвращаем дефолтное действие браузера
-    }
+    // Обработчик события касания вторым пальцем
+    document.addEventListener('touchstart', (e) => {
+      if (activeElement && e.touches.length === 2) {
+        flag = false; // Прерываем перетаскивание
+        activeElement.style.left = startPosition.left;
+        activeElement.style.top = startPosition.top;
+        activeElement = null; // Сбрасываем активный элемент
+        e.preventDefault(); // Предотвращаем дефолтное действие браузера
+      }
+    });
   });
 });
