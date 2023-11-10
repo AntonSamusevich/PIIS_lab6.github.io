a4fb3bc993121d0ebd97c408cc5decd0e8c6286d
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-10 13:02:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const targets = document.querySelectorAll('.target');
 
   let activeElement = null; // Активный элемент
-  let flag = false; // Состояние "следования за пальцем"
+  let flag1 = false; // Состояние "следования за пальцем"
+  let flag2 = false;
   let startPosition = null; // Исходная позиция элемента
   let touchCount = 0; // Счетчик касаний
   let touchStartTime = 0; // Время начала первого касания
@@ -10,50 +11,56 @@ document.addEventListener('DOMContentLoaded', function () {
   targets.forEach(target => {
     // Обработчик события касания начала
     target.addEventListener('touchstart', (e) => {
-      if (!activeElement) { // Если нет активного элемента, обрабатываем касание
-        const currentTime = new Date().getTime();
-        if (touchCount === 0 || (currentTime - touchStartTime < 300)) {
-          // Если прошло менее 0.3 секунды с начала первого касания, увеличиваем счетчик
-          touchCount++;
-          if (touchCount === 2) {
-            // Если счетчик достиг двух, считаем это двойным нажатием
-            touchCount = 0;
-            activeElement = target;
-            startPosition = {
-              left: target.style.left,
-              top: target.style.top,
-            };
-            activeElement.style.backgroundColor = 'green';
-            followingFinger = true; // Устанавливаем флаг "следования за пальцем"
-            
-            // Получаем координаты точки нажатия
-            const touch = e.touches[0];
-            const targetRect = activeElement.getBoundingClientRect();
-            const targetX = touch.clientX - targetRect.width / 2;
-            const targetY = touch.clientY - targetRect.height / 2;
-
-            // Анимация перемещения к точке нажатия
-            activeElement.style.transition = 'left 0.3s ease-out, top 0.3s ease-out';
-            activeElement.style.left = targetX + 'px';
-            activeElement.style.top = targetY + 'px';
-          } else {
-            touchStartTime = currentTime;
-          }
-        } else {
+      const currentTime = new Date().getTime();
+      if (!activeElement) {
+      if (touchCount === 0 || (currentTime - touchStartTime < 300)) {
+        // Если прошло менее 0.3 секунды с начала первого касания, увеличиваем счетчик
+        touchCount++;
+        if (touchCount === 2) {
+          // Если счетчик достиг двух, считаем это двойным нажатием
           touchCount = 0;
           activeElement = target;
           startPosition = {
             left: target.style.left,
             top: target.style.top,
           };
-          e.preventDefault();
+          activeElement.style.backgroundColor = 'green';
+          flag1 = true; // Устанавливаем флаг "следования за пальцем"
+        } else {
+          touchStartTime = currentTime;
         }
+      } else {
+        touchCount = 0;
+        activeElement = target;
+        startPosition = {
+          left: target.style.left,
+          top: target.style.top,
+        };
+        e.preventDefault();
+      }
+    }
+    });
+
+    // Обработчик события завершения касания
+    document.addEventListener('touchstart', (e) => {
+      if (flag1 && activeElement) {
+        // Получаем координаты точки нажатия
+        const touch = e.touches[0];
+        const targetRect = activeElement.getBoundingClientRect();
+        const targetX = touch.clientX - targetRect.width / 2;
+        const targetY = touch.clientY - targetRect.height / 2;
+
+        // Анимация перемещения к точке нажатия
+        activeElement.style.transition = 'left 0.3s ease-out, top 0.3s ease-out';
+        activeElement.style.left = targetX + 'px';
+        activeElement.style.top = targetY + 'px';
+        flag2 = true;
       }
     });
 
     // Обработчик события завершения касания
     document.addEventListener('touchstart', (e) => {
-      if (flag && activeElement) {
+      if (flag2 && activeElement) {
         if (e.changedTouches.length > 0) {
           const touch = e.changedTouches[0];
           // Перемещение элемента к точке отпускания пальца
