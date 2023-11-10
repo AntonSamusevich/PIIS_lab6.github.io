ccb835046c0f93bf146c1905827b7991c6fba560
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-10 12:55:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,42 +10,44 @@ document.addEventListener('DOMContentLoaded', function () {
   targets.forEach(target => {
     // Обработчик события касания начала
     target.addEventListener('touchstart', (e) => {
-      const currentTime = new Date().getTime();
-      if (touchCount === 0 || (currentTime - touchStartTime < 300)) {
-        // Если прошло менее 0.3 секунды с начала первого касания, увеличиваем счетчик
-        touchCount++;
-        if (touchCount === 2) {
-          // Если счетчик достиг двух, считаем это двойным нажатием
+      if (!activeElement) { // Если нет активного элемента, обрабатываем касание
+        const currentTime = new Date().getTime();
+        if (touchCount === 0 || (currentTime - touchStartTime < 300)) {
+          // Если прошло менее 0.3 секунды с начала первого касания, увеличиваем счетчик
+          touchCount++;
+          if (touchCount === 2) {
+            // Если счетчик достиг двух, считаем это двойным нажатием
+            touchCount = 0;
+            activeElement = target;
+            startPosition = {
+              left: target.style.left,
+              top: target.style.top,
+            };
+            activeElement.style.backgroundColor = 'green';
+            followingFinger = true; // Устанавливаем флаг "следования за пальцем"
+            
+            // Получаем координаты точки нажатия
+            const touch = e.touches[0];
+            const targetRect = activeElement.getBoundingClientRect();
+            const targetX = touch.clientX - targetRect.width / 2;
+            const targetY = touch.clientY - targetRect.height / 2;
+
+            // Анимация перемещения к точке нажатия
+            activeElement.style.transition = 'left 0.3s ease-out, top 0.3s ease-out';
+            activeElement.style.left = targetX + 'px';
+            activeElement.style.top = targetY + 'px';
+          } else {
+            touchStartTime = currentTime;
+          }
+        } else {
           touchCount = 0;
           activeElement = target;
           startPosition = {
             left: target.style.left,
             top: target.style.top,
           };
-          activeElement.style.backgroundColor = 'green';
-          flag = true; // Устанавливаем флаг "следования за пальцем"
-          
-          // Получаем координаты точки нажатия
-          const touch = e.touches[0];
-          const targetRect = activeElement.getBoundingClientRect();
-          const targetX = touch.clientX - targetRect.width / 2;
-          const targetY = touch.clientY - targetRect.height / 2;
-
-          // Анимация перемещения к точке нажатия
-          activeElement.style.transition = 'left 0.3s ease-out, top 0.3s ease-out';
-          activeElement.style.left = targetX + 'px';
-          activeElement.style.top = targetY + 'px';
-        } else {
-          touchStartTime = currentTime;
+          e.preventDefault();
         }
-      } else {
-        touchCount = 0;
-        activeElement = target;
-        startPosition = {
-          left: target.style.left,
-          top: target.style.top,
-        };
-        e.preventDefault();
       }
     });
 
