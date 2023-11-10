49c10c2767151a95e8fb02989bac5c7d36f1f2cd
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-10 11:07:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const targets = document.querySelectorAll('.target'); 
-  
+  const targets = document.querySelectorAll('.target');
+
   let activeElement = null; // Активный элемент
   let flag = false; // Состояние перемещения элемента
   let offsetX, offsetY; // Смещение относительно курсора
@@ -14,19 +14,18 @@ document.addEventListener('DOMContentLoaded', function () {
       const currentTime = new Date().getTime();
       pressStartTime = currentTime;
       if (touchCount === 0 || (currentTime - touchStartTime < 300)) {
+        // Если прошло менее 0.3 секунды с начала первого касания, увеличиваем счетчик
         touchCount++;
         if (touchCount === 2) {
-          activeElement.style.backgroundColor = 'green';
+          // Если счетчик достиг двух, считаем это двойным нажатием
           touchCount = 0;
-        activeElement = target;
-        startPosition = {
-          left: target.style.left,
-          top: target.style.top,
-        };
-        const touch = e.touches[0];
-        offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
-        offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
-        e.preventDefault();
+          activeElement = target;
+          startPosition = {
+            left: target.style.left,
+            top: target.style.top,
+          };
+          activeElement.style.backgroundColor = 'green';
+          flag = true; // Устанавливаем флаг "преследования"
         } else {
           touchStartTime = currentTime;
         }
@@ -43,20 +42,24 @@ document.addEventListener('DOMContentLoaded', function () {
         e.preventDefault();
       }
     });
-    
+
     // Обработчик события движения при касании
     document.addEventListener('touchmove', (e) => {
       if (activeElement) {
         const touch = e.touches[0];
-        activeElement.style.left = touch.clientX - offsetX + 'px'; 
-        activeElement.style.top = touch.clientY - offsetY + 'px';
-        e.preventDefault(); // Предотвращаем дефолтное действие браузера
+        if (flag) {
+          // Если установлен флаг "преследования", перемещаем элемент
+          activeElement.style.left = touch.clientX - offsetX + 'px';
+          activeElement.style.top = touch.clientY - offsetY + 'px';
+          e.preventDefault(); // Предотвращаем дефолтное действие браузера
+        }
       }
     });
 
     // Обработчик события завершения касания
     document.addEventListener('touchend', (e) => {
       activeElement = null; // Сбрасываем активный элемент
+      flag = false; // Сбрасываем флаг "преследования"
     });
   });
 });
