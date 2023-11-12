51fc961aa1a1d5eacd63315bc36efbcfbc4eb047
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-13 01:24:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
   let flag = false; 
   let offsetX, offsetY; 
   let startPosition = null; 
-  let touchCount = false; 
+  let touchCount = 0; 
   let touchStartTime = 0; 
   let holdTimer = null;
 
@@ -24,46 +24,31 @@ document.addEventListener('DOMContentLoaded', function () {
         const touch = e.touches[0];
         offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
         offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
-        activeElement.style.backgroundColor = 'red';
       }, 500);
 
       e.preventDefault();
     });
 
-    document.addEventListener('touchmove', (e) => {
-      if (activeElement) {
-        const touch = e.touches[0];
-        activeElement.style.left = touch.clientX - offsetX + 'px'; 
-        activeElement.style.top = touch.clientY - offsetY + 'px';
-        e.preventDefault();
-      }
-    });
+    
 
     document.addEventListener('touchend', (e) => {
       clearTimeout(holdTimer);
       const currentTime = new Date().getTime();
       const touchDuration = currentTime - touchStartTime;
-      if (touchCount == true) {
+      if (touchCount == 1) {
         touchCount = 0;
         activeElement = e.target;
         activeElement.style.backgroundColor = 'green';
         activeElement = null;
       }
       else if (touchDuration < 300) {
-        touchCount = true
+        touchCount++;
       } else {
-        touchCount = false;
+        touchCount = 0;
         activeElement = null;
       }
     });
 
-    document.addEventListener('touchstart', (e) => {
-      if (activeElement && e.touches.length === 2) {
-        activeElement.style.left = startPosition.left;
-        activeElement.style.top = startPosition.top;
-        activeElement = null; 
-        e.preventDefault(); 
-      }
-    });
+    
   });
 });
