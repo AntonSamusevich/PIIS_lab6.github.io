720d39ad5aae157c1481cc4e158e6397774d2287
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-13 02:15:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,35 @@ document.addEventListener('DOMContentLoaded', function () {
   let activeElement = null;
   let touchStartTime = 0;
   let holdTimer = null;
+  let clickCount = 0;
 
   targets.forEach(target => {
     target.addEventListener('touchstart', (e) => {
       const currentTime = new Date().getTime();
+      const elapsed = currentTime - touchStartTime;
+
+      if (elapsed < 300) {
+        // Если прошло менее 300 миллисекунд, считаем это быстрым двойным нажатием
+        clickCount++;
+      } else {
+        // В противном случае начинаем отсчет заново
+        clickCount = 1;
+      }
+
       touchStartTime = currentTime;
 
+      // Ожидаем 300 миллисекунд для следующего нажатия
       holdTimer = setTimeout(() => {
-        // Ваш код для обработки удержания
-        activeElement = target;
-        activeElement.style.backgroundColor = 'green';
-        console.log('Удержание');
-      }, 2000); // 2 секунды
+        if (clickCount === 2) {
+          // Ваш код для обработки быстрого двойного нажатия
+          activeElement = target;
+          activeElement.style.backgroundColor = 'blue';
+          console.log('Быстрое двойное нажатие');
+        }
+
+        // Сбрасываем счетчик
+        clickCount = 0;
+      }, 300);
 
       e.preventDefault();
     });
@@ -26,15 +43,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
     target.addEventListener('touchend', () => {
       clearTimeout(holdTimer); // Очищаем таймер при отпускании
-      const currentTime = new Date().getTime();
-      const touchDuration = currentTime - touchStartTime;
-
-      if (touchDuration < 300) {
-        // Ваш код для обработки быстрого нажатия
-        activeElement = target;
-        activeElement.style.backgroundColor = 'blue';
-        console.log('Быстрое нажатие');
-      }
     });
   });
 });
