c84b782ca0d7bec0d2bc65b9078aa4766d5cc234
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-12 23:55:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,60 @@
 document.addEventListener('DOMContentLoaded', function () {
   const targets = document.querySelectorAll('.target'); 
   
-  let activeElement = null;
+  let activeElement = null; 
+  let flag = false; 
   let offsetX, offsetY; 
   let startPosition = null; 
+  let touchCount = 0; 
+  let touchStartTime = 0; 
 
   targets.forEach(target => {
-
+    
     target.addEventListener('touchstart', (e) => {
       const currentTime = new Date().getTime();
       touchStartTime = currentTime;
-
+    
       holdTimer = setTimeout(() => {
-      activeElement = target;
-      startPosition = {
-        left: target.style.left,
-        top: target.style.top,
-      };
-      const touch = e.touches[0];
-      offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
-      offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
-      }, 300);
+        activeElement = target;
+        startPosition = {
+          left: target.style.left,
+           top: target.style.top,
+        };
+        const touch = e.touches[0];
+        offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
+        offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
+      }, 500);
 
-      e.preventDefault(); 
+      e.preventDefault();
     });
-  });
 
-  document.addEventListener('touchmove', (e) => {
-    if (activeElement) {
-      const touch = e.touches[0];
-      activeElement.style.left = touch.clientX - offsetX + 'px'; 
-      activeElement.style.top = touch.clientY - offsetY + 'px';
-      e.preventDefault(); 
-    }
-  });
+    document.addEventListener('touchmove', (e) => {
+      if (activeElement) {
+        const touch = e.touches[0];
+        activeElement.style.left = touch.clientX - offsetX + 'px'; 
+        activeElement.style.top = touch.clientY - offsetY + 'px';
+        e.preventDefault();
+      }
+    });
 
-  document.addEventListener('touchend', (e) => {
-    activeElement = null; 
-  });
+    document.addEventListener('touchend', (e) => {
+      clearTimeout(holdTimer);
+      const currentTime = new Date().getTime();
+      const touchDuration = currentTime - touchStartTime;
+      if (touchDuration < 300) {
+        activeElement = target;
+        activeElement.style.backgroundColor = 'green';
+      }
+      activeElement = null;
+    });
 
-  document.addEventListener('touchstart', (e) => {
-    if (activeElement && e.touches.length === 2) {
-      activeElement.style.backgroundColor = 'red';
-      activeElement.style.left = startPosition.left;
-      activeElement.style.top = startPosition.top;
-      activeElement = null; 
-      e.preventDefault(); 
-    }
+    document.addEventListener('touchstart', (e) => {
+      if (activeElement && e.touches.length === 2) {
+        activeElement.style.left = startPosition.left;
+        activeElement.style.top = startPosition.top;
+        activeElement = null; 
+        e.preventDefault(); 
+      }
+    });
   });
 });
