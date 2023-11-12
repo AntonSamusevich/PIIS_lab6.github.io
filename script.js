b57c0c20f3bba417ce67a27739c888ccf512bd1e
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-12 20:25:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
   const targets = document.querySelectorAll('.target'); 
   
-  let activeElement = null; // Активный элемент
-  let flag = false; // Состояние перемещения элемента
-  let offsetX, offsetY; // Смещение относительно курсора
-  let startPosition = null; // Исходная позиция элемента
-  let touchCount = 0; // Счетчик касаний
-  let touchStartTime = 0; // Время начала первого касания
+  let activeElement = null; 
+  let flag = false; 
+  let offsetX, offsetY; 
+  let startPosition = null; 
+  let touchCount = 0; 
+  let touchStartTime = 0; 
 
   targets.forEach(target => {
-    // Обработчик события касания начала
+    
     target.addEventListener('touchstart', (e) => {
-      const currentTime = new Date().getTime();
-      if (touchCount === 0 || (currentTime - touchStartTime < 300)) {
-        // Если прошло менее 0.3 секунды с начала первого касания, увеличиваем счетчик
-        touchCount++;
-        if (touchCount === 2) {
-          // Если счетчик достиг двух, считаем это двойным нажатием
-          touchCount = 0;
-          activeElement = target;
-          startPosition = {
-            left: target.style.left,
-            top: target.style.top,
-          };
-          activeElement.style.backgroundColor = 'green';
-        } else {
-          touchStartTime = currentTime;
-        }
-      } else {
-        touchCount = 0;
         activeElement = target;
         startPosition = {
           left: target.style.left,
@@ -38,32 +20,28 @@ document.addEventListener('DOMContentLoaded', function () {
         offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
         offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
         e.preventDefault(); 
-      }
     });
 
-    // Обработчик события движения при касании
     document.addEventListener('touchmove', (e) => {
       if (activeElement) {
         const touch = e.touches[0];
         activeElement.style.left = touch.clientX - offsetX + 'px'; 
         activeElement.style.top = touch.clientY - offsetY + 'px';
-        e.preventDefault(); // Предотвращаем дефолтное действие браузера
+        e.preventDefault();
       }
     });
 
-    // Обработчик события завершения касания
     document.addEventListener('touchend', (e) => {
-      activeElement = null; // Сбрасываем активный элемент
+      activeElement = null; 
     });
 
-    // Обработчик события касания вторым пальцем
     document.addEventListener('touchstart', (e) => {
       if (activeElement && e.touches.length === 2) {
         flag = false; // Прерываем перетаскивание
         activeElement.style.left = startPosition.left;
         activeElement.style.top = startPosition.top;
-        activeElement = null; // Сбрасываем активный элемент
-        e.preventDefault(); // Предотвращаем дефолтное действие браузера
+        activeElement = null; 
+        e.preventDefault(); 
       }
     });
   });
