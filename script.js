0318773074ac68a3fd9456970e607f24b4ab1948
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-12 19:49:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,10 +20,6 @@ document.addEventListener('DOMContentLoaded', function () {
       e.preventDefault();
     });
 
-    target.addEventListener('touchmove', (e) => {
-      clearTimeout(holdTimer); // Очищаем таймер при движении
-    });
-
     target.addEventListener('touchend', () => {
       clearTimeout(holdTimer); // Очищаем таймер при отпускании
       const currentTime = new Date().getTime();
