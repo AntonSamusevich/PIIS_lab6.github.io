5dc00e53da2f6897af3b1b4b117fc6ae6cc070e3
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-13 12:32:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -44,14 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
       e.preventDefault();
     });
 
-    document.addEventListener('touchmove', (e) => {
-      if (activeElement && flagMove === true) {
-        const touch = e.touches[0];
-        activeElement.style.left = touch.clientX - offsetX + 'px'; 
-        activeElement.style.top = touch.clientY - offsetY + 'px';
-        e.preventDefault();
-      }
-    });
+    
 
     document.addEventListener('touchstart', (e) => {
       if (activeElement && flag === true) {
