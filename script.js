6876fb823f09f71060835aa371c73e87515e0d1c
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-13 11:41:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ document.addEventListener('DOMContentLoaded', function () {
             activeElement.style.transition = 'left 0.3s ease-out, top 0.3s ease-out';
             activeElement.style.left = targetX + 'px';
             activeElement.style.top = targetY + 'px';
-            flag = true; // Устанавливаем флаг в true после перемещения
+           
           }
     
           clickCount = 0;
