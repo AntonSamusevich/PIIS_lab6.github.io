a689990fe7fdf86001f6f195ffa8c4b2c690785a
AntonSamusevich <144314583+AntonSamusevich@users.noreply.github.com>
2023-11-13 21:20:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,97 +1,98 @@
 document.addEventListener('DOMContentLoaded', function () {
   const targets = document.querySelectorAll('.target');
 
-  let activeElement = null;
-  let touchStartTime = 0;
-  let holdTimer = null;
-  let clickCount = 0;
-  let flag = false;
-  let flagMove = false;
+  let activeElement = null; //активный элемент
+  let touchStartTime = 0; //время начала касания
+  let holdTimer = null; //таймер удержания
+  let clickCount = 0; //счетчик кликов
+  let flag = false; //флаг режима 
 
   targets.forEach(target => {
 
-    //обработчик события начала касания
+    //Обработчик события начала касания
     target.addEventListener('touchstart', (e) => {
       if (!activeElement) {
+        const currentTime = new Date().getTime();
+        const touchDuration = currentTime - touchStartTime;
 
-      const currentTime = new Date().getTime();
-      const touchDuration = currentTime - touchStartTime;
+        if (touchDuration < 300) {  
+          clickCount++;
+        } else {
+          clickCount = 1;
+        }
 
-      if (touchDuration < 300) {
-        clickCount++;
-      } else {
-        clickCount = 1;
+        touchStartTime = currentTime;
+
+        holdTimer = setTimeout(() => {
+          if (clickCount === 2) { //двойное нажатие
+            activeElement = target;
+            activeElement.style.backgroundColor = 'green';
+            flag = true;
+          } else {
+            activeElement = e.target;
+            startPosition = {
+              left: target.style.left,
+              top: target.style.top,
+            };
+            //объект первого касания из массива
+            const touch = e.touches[0];
+            //координаты места касания
+            offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
+            offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
+            flagMove = true;
+          }
+          clickCount = 0;
+        }, 300);
+
+        e.preventDefault();
       }
-
-      touchStartTime = currentTime;
-
-      holdTimer = setTimeout(() => {
-        if (clickCount === 2) {
-          activeElement = target;
-          activeElement.style.backgroundColor = 'green';
-          flag = true;
-        }
-        else { 
-          activeElement = e.target;
-          startPosition = {
-            left: target.style.left,
-            top: target.style.top,
-          };
-          const touch = e.touches[0];
-          offsetX = touch.clientX - activeElement.getBoundingClientRect().left;
-          offsetY = touch.clientY - activeElement.getBoundingClientRect().top;
-          flagMove = true;
-        }
-        clickCount = 0;
-      }, 300);
-
-      e.preventDefault();
-    }
     });
 
     //обработчик события движения при касании
     document.addEventListener('touchmove', (e) => {
       if (activeElement && flagMove === true) {
         const touch = e.touches[0];
+        //смещение относительно начального положения
         activeElement.style.left = touch.clientX - offsetX + 'px'; 
         activeElement.style.top = touch.clientY - offsetY + 'px';
         e.preventDefault();
       }
     });
 
-    //обработчик события режима «следующий за пальцем» 
+    //обработчик события режима "следующий за пальцем" 
     document.addEventListener('touchstart', (e) => {
       if (activeElement && flag === true) {
-
         const currentTime = new Date().getTime();
         const touchDuration = currentTime - touchStartTime;
-    
+
         if (touchDuration < 300) {
           clickCount = 1;
           flag = false;
         } else {
           clickCount = 0;
         }
-    
+
         touchStartTime = currentTime;
-    
+
         holdTimer = setTimeout(() => {
           if (clickCount === 1) {
             flag = false;
-          }
-          else {
+          } else {
+            
+            //получаем координаты точки нажатия
             const touch = e.touches[0];
             const targetRect = activeElement.getBoundingClientRect();
             const targetX = touch.clientX - targetRect.width / 2;
             const targetY = touch.clientY - targetRect.height / 2;
 
+            //анимация перемещения к точке нажатия
             activeElement.style.transition = 'left 0.3s ease-out, top 0.3s ease-out';
             activeElement.style.left = targetX + 'px';
             activeElement.style.top = targetY + 'px';
-            flag = true; 
+            flag = true;
           }
           clickCount = 0;
-        }, 500);
+        }, 300);
       }
     });
     
@@ -103,8 +104,8 @@ document.addEventListener('DOMContentLoaded', function () {
         activeElement = null;
         flag = false;
       } else {
-      activeElement = null;
-      flagMove = false;
+        activeElement = null;
+        flagMove = false;
       }
     });
 
